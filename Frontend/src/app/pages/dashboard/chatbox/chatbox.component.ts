7ea7b426cb98dbceb4b4a57b4af67a238d46812c
Sanjay S <SanjaySasimandir@users.noreply.github.com>
2022-05-01 14:24:35 +0530

    sending messages to server done

--- Frontend/src/app/pages/dashboard/chatbox/chatbox.component.ts
+++ Frontend/src/app/pages/dashboard/chatbox/chatbox.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { UsersChannelModel } from 'src/app/models/group.model';
+import { SendMessageModel } from 'src/app/models/sendmessage.model';
+import * as moment from "moment";
+import { WebSocketService } from 'src/app/services/web-socket.service';
+import { MessageSender } from 'src/app/models/sendmessage.model';
 
 @Component({
   selector: 'app-chatbox',
@@ -10,9 +15,50 @@ export class ChatboxComponent implements OnInit {
 
   @Input() selectedChannel = new UsersChannelModel('', '', '', '');
 
-  constructor() { }
+  constructor(private webSocket: WebSocketService) { }
+
+  uploadToggle = false;
+
+  selectedImage!: FileList;
+  imageSelected(element: any) {
+    this.selectedImage = element.target.files;
+    console.log(this.selectedImage)
+  }
+
+  sendImageSelected() {
+    var file = this.selectedImage[0];
+    var reader = new FileReader();
+    reader.onload = this.readerLoaded.bind(this);
+    reader.readAsBinaryString(file);
+  }
+
+  readerLoaded(evt: any) {
+    var binaryString = evt.target.result;
+  }
+
+
+  username = "" + localStorage.getItem('username');
+  fullname = "" + localStorage.getItem('fullname');
+  newMessage = new FormControl("");
+  sendMessage() {
+    let newMessage = new SendMessageModel(
+      this.selectedChannel.channelid,
+      'text',
+      this.newMessage.value,
+      moment().format('LT'),
+      new MessageSender(
+        this.username,
+        this.fullname,
+      ));
+    this.webSocket.emit('send message', newMessage);
+  }
+
+  socketListeners() {
+    
+  }
 
   ngOnInit(): void {
+    this.socketListeners();
   }
 
 }
